55710fe8b805ea19bb3c74d9adcf964cc84cee8b
Utkarsha Nikam <utkarsha.nikam@waiin.com>
2025-02-05 17:34:46 +0530

    seconde changes

--- temp.js
+++ temp.js
@@ -1,3 +1,10 @@
 // document.getElementById("ang").addEventListener("click", function() {
 //     document.getElementsByClassName("subdash").style.display = "block";
 // });
+
+  var quill = new Quill('#editor', {
+    theme: 'snow'  // 'bubble' theme is also available
+  });
+
+
+//   -----------------------------------------CV
